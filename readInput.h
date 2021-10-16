0a8c72dfe922f676f4126d464689dbc1582a7b03
Justus <92258644+Justus03@users.noreply.github.com>
2021-10-16 10:12:58 +0000

    Update readInput.h

--- readInput.h
+++ readInput.h
@@ -11,5 +11,14 @@ union type_3
 typedef union type_3 type_3;
 void voidBuffer();
 type_3 *readInput(char *Chaine,int taille,char mode,type_3 *Variable);
+/* 
+*description de la fonction*
+Cette fonction prends en paramètre 4 éléments 
+* élément 1: la chaîne dans laquelle écrire
+* élément 2: la taille de la chaîne
+* élément 3: le genre d'écriture... 'd' pour un double, 'l' pour un long, 's' pour une chaine de caractère.
+* élément 4: La variable de type type_3 a utilisé. Pour la première utilisation envoyé NULL et pour les autres utilisations utilisées l'adresse renvoyée par la fonction lors de la première utilisation.
+* un exemple d'utilisation a été fournie en commentaire dans le fichier readInput.c. Please uncomment to test.
+*/
 
 #endif
